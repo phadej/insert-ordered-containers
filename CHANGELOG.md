0eb30b91e58d44faf5cdad9f35b4cc9771a6dd81
Oleg Grenrus <oleg.grenrus@iki.fi>
2019-05-17 10:08:57 +0300

    Add InsOrdHashSet

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,6 @@
+- 0.2.2
+    - Add `Data.HashSet.InsOrd`
+
 - 0.2.1.0
     - Fix `Traversable`, `TraversableWithIndex`, `FoldableWithIndex` to traverse
       in insertion order
--- src/Data/HashMap/Strict/InsOrd.hs
+++ src/Data/HashMap/Strict/InsOrd.hs
@@ -84,7 +84,7 @@ module Data.HashMap.Strict.InsOrd (
 import Prelude        ()
 import Prelude.Compat hiding (filter, foldr, lookup, map, null)
 
-import           Control.Applicative             (Const (..), (<**>))
+import           Control.Applicative             (Const (..))
 import           Control.Arrow                   (first, second)
 import           Data.Aeson
 import qualified Data.Aeson.Encoding             as E
@@ -113,6 +113,8 @@ import qualified Data.HashMap.Strict as HashMap
 #if MIN_VERSION_base(4,7,0)
 import qualified GHC.Exts as Exts
 #endif
+
+import Data.HashMap.InsOrd.Internal
 
 -------------------------------------------------------------------------------
 -- Strict Pair Int a
@@ -419,37 +421,6 @@ foldMapWithKey f = foldMap (uncurry f) . toList
 traverseWithKey :: Applicative f => (k -> a -> f b) -> InsOrdHashMap k a -> f (InsOrdHashMap k b)
 traverseWithKey f (InsOrdHashMap n m) = InsOrdHashMap n <$> retractSortedAp
     (HashMap.traverseWithKey (\k (P i v) -> liftSortedAp i (P i <$> f k v)) m)
-
--- Sort using insertion sort
--- Hopefully it's fast enough for where we need it
--- otherwise: https://gist.github.com/treeowl/9621f58d55fe0c4f9162be0e074b1b29
--- http://elvishjerricco.github.io/2017/03/23/applicative-sorting.html also related
-
--- Free applicative which re-orders effects
--- Mostly from Edward Kmett's `free` package.
-data SortedAp f a where
-    Pure :: a -> SortedAp f a
-    SortedAp   :: !Int -> f a -> SortedAp f (a -> b) -> SortedAp f b
-
-instance Functor (SortedAp f) where
-    fmap f (Pure a)   = Pure (f a)
-    fmap f (SortedAp i x y)   = SortedAp i x ((f .) <$> y)
-
-instance Applicative (SortedAp f) where
-    pure = Pure
-    Pure f <*> y = fmap f y
-    -- This is different from real Ap
-    f <*> Pure y = fmap ($ y) f
-    f@(SortedAp i x y) <*> z@(SortedAp j u v)
-        | i < j     = SortedAp i x (flip <$> y <*> z)
-        | otherwise = SortedAp j u ((.) <$> f <*> v)
-
-liftSortedAp :: Int -> f a -> SortedAp f a
-liftSortedAp i x = SortedAp i x (Pure id)
-
-retractSortedAp :: Applicative f => SortedAp f a -> f a
-retractSortedAp (Pure x) = pure x
-retractSortedAp (SortedAp _ f x) = f <**> retractSortedAp x
 
 -------------------------------------------------------------------------------
 -- Unordered
